b14cb46f83f47242c02ee0edfbc16d08239e37df
Esotuofgr7 <kachiesotu@gmail.com>
2025-09-14 18:28:03 +0100

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,32 @@
+
+
+
+
+from tkinter import *
+
+window = Tk()
+window.title('Tkinter Sample Window')
+window.geometry('300x300')
+
+# Label
+greeting = Label(text="Hello User", fg='black', bg='white')
+
+# Button
+button = Button(text="Click Me", bg='Black', fg='White')
+
+# Entry
+entry = Entry(fg="yellow", bg="blue", width=50)
+
+greeting.pack()
+button.pack()
+entry.pack()
+
+frame = Frame(master=window, relief=RAISED, borderwidth=5)
+frame.pack()
+
+label = Label(master=frame, text='Sample Frame')
+label.pack()
+
+textbox = Text(fg='green', bg='yellow')
+textbox.pack()
+window.mainloop()
